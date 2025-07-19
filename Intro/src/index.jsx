9c2c25ed2923a6a9aa7cdba4fc602b8b5908f682
deepuchary03 <deepuchary03@gmail.com>
2025-07-19 19:19:56 +0530

    a

--- Intro/src/index.jsx
+++ Intro/src/index.jsx
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom";
 ReactDOM.render(
   <div>
     <h1>Hello World!</h1>
-    <p>This is a paragraph.</p>
+    <p>This is a paragraph. </p>
   </div>,
   document.getElementById("root")
 );
